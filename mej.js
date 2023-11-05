29c0a83f43813dbccd8a4c69aceed1ddd7d58b7b
BakosKristof <94175203+BakosKristof@users.noreply.github.com>
2023-11-05 16:53:53 +0100

    Add files via upload

---
+++ mej.js
@@ -0,0 +1,35 @@
+var jsonAdatok;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "mejson.json", true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        jsonAdatok = xhr.responseText;
+        data.forEach(i => {
+            document.getElementById("honnan").innerHTML += `<li onclick="hozzaad(this, 'honnanvalaszt')">${i.honnan}</li>`;
+            document.getElementById("hova").innerHTML += `<li onclick="hozzaad(this, 'hovavalaszt')">${i.hova}</li>`;
+        })
+
+    }
+};
+xhr.send();
+
+function hozzaad(item, value) {
+    document.getElementById(value).innerHTML = item.textContent;
+
+}
+function keres() {
+    var honnan = document.getElementById("honnanvalaszt").textContent;
+    var hova = document.getElementById("hovavalaszt").textContent;
+    var adatok = JSON.parse(jsonAdatok);
+    var van = false
+    adatok.forEach(i => {
+        if (i.honnan == honnan && i.hova == hova) {
+            document.getElementById("eredmeny").innerHTML = i.indulas + "------------" + i.erkezes;
+            van = true;
+        }
+    });
+    if (!van) {
+        return alert("Nem található adat!")
+    }
+}
